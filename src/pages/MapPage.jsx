68f321e944839bedb0f4d4714bdefda7753244c2
Saiteja <gudapalavsteja1526@gmail.com>
2026-05-16 13:36:27 +0530

    updated frontend

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -2,12 +2,9 @@ import MapView from "../MapView";
 
 export default function MapPage({ signals, target }) {
   return (
-    <div className="card">
-      <h2>🗺️ Live Traffic Map</h2>
-
-      <div style={{ height: "500px" }}>
-        <MapView signals={signals} target={target} />
-      </div>
+    <div>
+      <h2>🗺️ Map</h2>
+      <MapView signals={signals} target={target} />
     </div>
   );
 }
